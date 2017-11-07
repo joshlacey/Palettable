71481400659987b87a3985cd72a5d37fb5ac56e4
joshlacey <williamjoshualacey@gmail.com>
2017-11-06 22:13:51 -0500

    edited SVGElement

--- src/component/palate/SVGElement.js
+++ src/component/palate/SVGElement.js
@@ -4,56 +4,36 @@ import addHandleFunc from '../../snap/scale.js';
 import { start, move, stop } from '../../snap/dragCallbacks.js';
 import { connect } from 'react-redux';
 
-
 class SVGElement extends React.Component {
 
-  componentDidMount = () => {
-
-    //Creates object from svg created on first render.
-    const s = Snap(`#${this.props.id}`)
-
-    let myCircle = s.circle(50,50,50).attr({ fill: this.props.fill, transform: this.props.position })
-    myCircle.mouseover(() => this.props.hoverData(this.props.id))
+  snapAndAddEventListeners (props) {
+    const s = Snap(`#${props.id}`)
+    let myCircle = s.circle(50,50,50).attr({ fill: props.fill, transform: props.position })
+    myCircle.mouseover(() => this.props.hoverData(props.id))
     myCircle.drag( move, start, stop )
     let handleGroup = s.group(myCircle)
-    handleGroup.attr({ transform: this.props.size })
-    //myCircle.dblclick(() => {handleGroup.remove(); addHandleFunc( myCircle, s, handleGroup, false)} )
-
-
-    if(this.props.reorderMode) {
-      myCircle.dblclick(() => this.props.reorder(myCircle, this.props.id))
-    } else if (this.props.deleteMode) {
-      myCircle.dblclick(() => this.props.deleteEl(myCircle, this.props.id))
+    handleGroup.attr({ transform: props.size })
+    if(props.reorderMode) {
+      myCircle.undblclick()
+      myCircle.dblclick(() => this.props.reorder(myCircle, props.id))
+    } else if (props.deleteMode) {
+      myCircle.undblclick()
+      myCircle.dblclick(() => this.props.deleteEl(myCircle, props.id))
     } else {
+      myCircle.undblclick()
       myCircle.dblclick(() => {handleGroup.remove(); addHandleFunc( myCircle, s, handleGroup, false)} )
     }
+  }
 
+  componentDidMount = () => {
+    this.snapAndAddEventListeners(this.props)
   }
 
   componentWillReceiveProps = (nextProps) => {
-
-    //Creates object from svg created on first render.
+    //remove any elements currently in the parent SVG
     document.getElementById(nextProps.id).innerHTML = ""
-    const s = Snap(`#${nextProps.id}`)
-
-    let myCircle = s.circle(50,50,50).attr({ fill: nextProps.fill, transform: nextProps.position })
-    myCircle.mouseover(() => this.props.hoverData(nextProps.id))
-    myCircle.drag( move, start, stop )
-    let handleGroup = s.group(myCircle)
-    handleGroup.attr({ transform: nextProps.size })
-    if(nextProps.reorderMode) {
-      myCircle.undblclick()
-      myCircle.dblclick(() => this.props.reorder(myCircle, nextProps.id))
-    } else if (nextProps.deleteMode) {
-      myCircle.undblclick()
-      myCircle.dblclick(() => this.props.deleteEl(myCircle, nextProps.id))
-    } else {
-      myCircle.undblclick()
-      myCircle.dblclick(() => {handleGroup.remove(); addHandleFunc( myCircle, s, handleGroup, false)} )
-    }
+    this.snapAndAddEventListeners(nextProps)
   }
-
-
 
   render() {
     return (
@@ -62,9 +42,12 @@ class SVGElement extends React.Component {
   }
 }
 
+//setting this equal to an undefined value in the palate state seems to work for some reason.
+//if I change it to any other value the component breaks.
+//if I remove connect the component breaks 
 function mapStateToProps(state) {
   return {
-    currentPalate: state.palate.current
+    current: state.palate.current
   }
 }
 
