eb22a6e3a33b043f76af4bf110ed5b8d4d554664
joshlacey <williamjoshualacey@gmail.com>
2017-12-23 18:17:55 -0500

    url validation to image uploader

--- src/component/uploader/ImageUploader.js
+++ src/component/uploader/ImageUploader.js
@@ -6,34 +6,43 @@ import '../../index.css'
 class ImageUploader extends React.Component {
 
   state = {
-    url: ""
+    url: "",
+    valid_url: true
   }
 
   handleChange = (event) => {
     event.preventDefault()
-    this.setState({
-      url: event.target.value
-    })
+    let string = event.target.value
+    if (/\.(?:jpe?g|png|gif|bmp|raw|ico|webp)/.test(string) || !string.length){
+      this.setState({
+        url: event.target.value,
+        valid_url: true
+      })
+    } else {
+      this.setState({
+        valid_url: false
+      })
+    }
   }
 
   handleSubmit = (event) => {
     event.preventDefault()
-    if(this.state.url !== "") {
+    if(this.state.url !== "" && this.state.valid_url) {
       this.props.searchColors(this.state.url)
     } else {
       alert("Please enter a valid url")
     }
-
   }
 
   render() {
-
+    let errorMessage = this.state.valid_url ? '' : '* Invalid Url Please Try Again'
     return (
       <div className={'image-form'}>
         <form onSubmit={this.handleSubmit}>
-          <input onChange={this.handleChange} placeholder={'Image URL'}/>
+          <input maxlength='500' onChange={this.handleChange} placeholder={'Image Url'}/>
           <button className={'nice-button'} type='submit'>Sample ></button>
         </form>
+        <div><p style={{color: 'red', fontSize: '.5em'}}>{errorMessage}</p></div>
       </div>
     )
   }
